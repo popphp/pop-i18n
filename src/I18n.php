c21d32aee715de65d1578faa1baab51fe289d77e
Nick Sagona <nick@noladev.com>
2025-10-30 10:58:49 -0500

    Update version and copyright

--- src/I18n.php
+++ src/I18n.php
@@ -4,7 +4,7 @@
  *
  * @link       https://github.com/popphp/popphp-framework
  * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2025 NOLA Interactive, LLC.
+ * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
  */
 
@@ -21,9 +21,9 @@ use SimpleXMLElement;
  * @category   Pop
  * @package    Pop_I18n
  * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2025 NOLA Interactive, LLC.
+ * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
- * @version    4.0.2
+ * @version    4.0.3
  */
 class I18n
 {
